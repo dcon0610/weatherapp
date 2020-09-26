71c76f25e7c0499b2cb70ac5f49db3c1b49e4930
David Connell <davecon88@gmail.com>
2020-09-26 23:15:41 +0930

    final submission

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,28 @@
 $(document).ready(function(){
     var APIkey = "da435f659ec26288eb1c90b766251c6f"
- 
+    var latitude =""
+    var longitude =""
     j=0
+    var k=0
+    var listItem=[]
+
+      
+    for (i=1;i<localStorage.length+1; i++){
+      listItem[i]=localStorage.getItem(i)
+      console.log("listitem",listItem[i])
+      var history = $("<li>", {id: i+"history"});
+     
+      $('#search_history').append(history)
+      $("#"+i+"history").html(listItem[i])
+} 
     
     $('#search').keypress(function (e) {
         var key = e.which;
         if(key == 13){
             var city=$('#search').val()
+            k=localStorage.length
+            localStorage.setItem(k+1,city)
+            
            
     $.ajax({
         url: "http://api.openweathermap.org/data/2.5/weather?q=" + city +"&appid="+APIkey,
@@ -29,14 +45,30 @@ $(document).ready(function(){
          $('#temperature').html("Temperature: " + Math.round(response.main.temp-273.15) +" °C")
         $('#humidity').html("Humidity: " + response.main.humidity+" %")
         $('#wind_speed').html("Wind Speed: " + response.wind.speed + " MPH")
+        latitude = response.coord.lat
+        longitude = response.coord.lon
+        console.log(latitude, longitude)
         
+     
+      
+      $.ajax({
+        url: "http://api.openweathermap.org/data/2.5/uvi?lat=" + latitude  +"&lon=" + longitude +"&appid=" + APIkey,
+        method: "GET",
+        error: function (xhr){
+          if(xhr.status !==200) {
+            $("#UV_index").empty()
+      }}}).then(function(response) {
+        $('#UV_index').html("UV Index: " + response.value)
       })
-
+    })
 
       $.ajax({
         url: "http://api.openweathermap.org/data/2.5/forecast/?q=" + city +"&appid="+APIkey,
-        method: "GET"
-      }).then(function(response) {
+        method: "GET",
+        error: function (xhr){
+          if(xhr.status !==200) {
+            $("#cards").empty()
+      }}}).then(function(response) {
        
         console.log(response.list)
         console.log(moment("2020-09-26 09:00:00").format('HH'))
@@ -76,17 +108,17 @@ $(document).ready(function(){
         $("#"+i).append($insidediv);
         }
       
-        
+      
      
       })
-
-
+      
+    
 
 
         
-      }
+      
 
-
+    }
     })
 
 
@@ -96,3 +128,4 @@ $(document).ready(function(){
 
 
     })
+  
